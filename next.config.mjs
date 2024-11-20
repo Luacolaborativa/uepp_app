02fc97e952429e88ec26c05a5cc03bbe6adef017
gesiel lucas <gesiel.lucas@hotmail.com>
2024-11-20 16:13:52 -0300

    fix: the reason for the problem in authentication was the proxy.

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'http://127.0.0.1:8000/api/:path*' // Proxy to Backend
-            }
-        ]
-    },
-    devServer: {
-        port: 8000 // Specify the port you want to use
-    }
+   
 };
 
 export default nextConfig;
